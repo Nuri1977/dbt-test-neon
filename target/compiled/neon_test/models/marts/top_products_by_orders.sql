0a05000af82590d04a4dbceefc23ca5bc749d305
Nuri Lacka <nurilacka@gmail.com>
2026-06-10 16:53:30 +0200

    ne chnages

---
+++ target/compiled/neon_test/models/marts/top_products_by_orders.sql
@@ -0,0 +1,22 @@
+with order_items as (
+    select * from "dbt-test-4444"."public"."public_order_items"
+),
+
+products as (
+    select * from "dbt-test-4444"."public"."public_products"
+),
+
+product_order_counts as (
+    select
+        product_id,
+        count(order_id) as total_orders
+    from order_items
+    group by 1
+)
+
+select
+    p.name,
+    poc.total_orders
+from product_order_counts poc
+join products p on poc.product_id = p.product_id
+order by poc.total_orders desc
